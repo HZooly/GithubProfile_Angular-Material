665cf2d3e546bc47b35609a07660aacc0514f6c6
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2017-07-27 05:44:34 +0200

    fetching github user data from Github API and display it in card

--- client/app/components/home/home.controller.js
+++ client/app/components/home/home.controller.js
@@ -1,21 +1,49 @@
 angular.module('githubMaterial').controller('homeController', homeController);
 
-homeController.$inject = [];
+homeController.$inject = ['$http', '$mdToast'];
 
-function homeController() {
+function homeController($http, $mdToast) {
 	let me = this;
 
 	me.current;
+	me.searchUser;
+	me.user;
 
 	me.init = init;
 	me.setCurrent = setCurrent;
+	me.search = search;
+	me.fetchData = fetchData;
 
-	function init(){
-		me.current = 'User';
+	function init() {
+		me.searchUser = 'torzuoliH';
+		search();
+		me.setCurrent('User');
 	}
 
-	function setCurrent(tab){
+	function setCurrent(tab) {
 		me.current = tab;
+	}
+
+	function search() {
+		$http({
+			method: 'GET',
+			url: 'https://api.github.com/users/' + me.searchUser
+		}).then(function(response) {
+			fetchData(response.data);
+		}).catch(function(error) {
+			$mdToast.show(
+				$mdToast.simple()
+				.textContent('User not found!')
+				.position('bottom')
+				.hideDelay(3000)
+			);
+		});
+	}
+
+	function fetchData(data) {
+		console.log(data);
+		me.user = data;
+		me.setCurrent('User');
 	}
 
 	init();
