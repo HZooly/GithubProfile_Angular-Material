833a3550fdeffb82af3c214b1c69678a1b9a69b9
Hugo Torzuoli <torzuoli.hugo@gmail.com>
2017-08-01 21:21:29 +0200

    Followers - Circle loading

--- client/app/components/home/tabs/user/dialog/followers.dialog.controller.js
+++ client/app/components/home/tabs/user/dialog/followers.dialog.controller.js
@@ -8,21 +8,26 @@ function followersDialogController($scope, $mdDialog, $http, urlFollowers, userL
 	me.list;
 	me.url;
 	me.followerLink;
+	me.followersLoading;
 
 	me.init = init;
+	me.getFollowers = getFollowers;
 
 	function init(){
 		me.url = urlFollowers;
+		me.followersLoading = false;
 		me.followerLink = 'https://github.com/' + userLogin + '?tab=followers';
 		getFollowers();
 	}
 
 	function getFollowers(){
+		me.followersLoading = true;
 		$http({
 			method: 'GET',
 			url: me.url
 		}).then(function(response){
 			console.log(response);
+			me.followersLoading = false;
 			me.list = response.data;
 		}).catch(function(error){
 			console.log(error);
